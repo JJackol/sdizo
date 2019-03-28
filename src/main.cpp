945f038d2157128f4874d5ed7f214e1298684ec4
Jacek Czerwonka <jacekczerwonka@gmail.com>
2019-03-28 15:00:36 +0100

    new file struct

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "sdizo/main_list.h"
+
+using namespace std;
+
+int main(int argc, char** argv)
+{
+    cout << "Program: Struktury" << endl;
+
+    main_list(argc, argv);
+
+    return 0;
+}
