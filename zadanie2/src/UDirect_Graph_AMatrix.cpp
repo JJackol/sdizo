27b6b9d77aba40107feb3985d3b6019530e02024
Jacek Czerwonka <jacekczerwonka@gmail.com>
2019-05-19 13:48:38 +0200

    Adjacency matrix

---
+++ zadanie2/src/UDirect_Graph_AMatrix.cpp
@@ -0,0 +1,11 @@
+#include "UDirect_Graph_AMatrix.h"
+
+UDirect_Graph_AMatrix::UDirect_Graph_AMatrix()
+{
+	//ctor
+}
+
+UDirect_Graph_AMatrix::~UDirect_Graph_AMatrix()
+{
+	//dtor
+}
---
+++ zadanie2/src/UDirect_Graph_AMatrix.h
@@ -0,0 +1,16 @@
+#ifndef UDIRECT_GRAPH_AMATRIX_H
+#define UDIRECT_GRAPH_AMATRIX_H
+
+
+class UDirect_Graph_AMatrix
+{
+	public:
+		UDirect_Graph_AMatrix();
+		virtual ~UDirect_Graph_AMatrix();
+
+	protected:
+
+	private:
+};
+
+#endif // UDIRECT_GRAPH_AMATRIX_H
