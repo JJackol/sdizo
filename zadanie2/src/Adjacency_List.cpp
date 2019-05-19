fcbaef28e0607cb430a0e3012fe516fdbe05e6db
Jacek Czerwonka <jacekczerwonka@gmail.com>
2019-05-19 13:51:43 +0200

    List struct 0

---
+++ zadanie2/src/Adjacency_List.cpp
@@ -0,0 +1,11 @@
+#include "Adjacency_List.h"
+
+Adjacency_List::Adjacency_List()
+{
+	//ctor
+}
+
+Adjacency_List::~Adjacency_List()
+{
+	//dtor
+}
---
+++ zadanie2/src/Adjacency_List.h
@@ -0,0 +1,16 @@
+#ifndef ADJACENCY_LIST_H
+#define ADJACENCY_LIST_H
+
+
+class Adjacency_List
+{
+	public:
+		Adjacency_List();
+		virtual ~Adjacency_List();
+
+	protected:
+
+	private:
+};
+
+#endif // ADJACENCY_LIST_H
