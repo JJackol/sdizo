e92a65aa8ffe828054c065568b1ccaf0dc2db284
Jacek Czerwonka <jacekczerwonka@gmail.com>
2019-05-19 11:01:34 +0200

    Graph Interface 0

---
+++ zadanie2/src/Graph_I.cpp
@@ -0,0 +1,11 @@
+#include "Graph_I.h"
+
+Graph_I::Graph_I()
+{
+	//ctor
+}
+
+Graph_I::~Graph_I()
+{
+	//dtor
+}
---
+++ zadanie2/src/Graph_I.h
@@ -0,0 +1,16 @@
+#ifndef GRAPH_I_H
+#define GRAPH_I_H
+
+
+class Graph_I
+{
+	public:
+		Graph_I();
+		virtual ~Graph_I();
+
+	protected:
+
+	private:
+};
+
+#endif // GRAPH_I_H
